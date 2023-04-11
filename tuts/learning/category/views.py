e56c536c2da2481ddb63da7a0cd13454c9e9bd89
aiyappa <109279579+cypherAiyappa@users.noreply.github.com>
2023-04-11 06:33:50 +0530

    serializers and views

--- tuts/learning/category/views.py
+++ tuts/learning/category/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+import json
+from django.forms.models import model_to_dict
+# from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from products.models import Product
+from products.serializers import ProductSerializer
 
-# Create your views here.
+@api_view(["GET"])
+def api_home(request, *args, **kwargs):
+    serializer = ProductSerializer(data = request.data)
+    if serializer.is_valid(raise_exception=True):
+        print(serializer.data)
+        data = serializer.data
+        return Response(data)
